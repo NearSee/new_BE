a01c794801d9432140dba44b48252f5bacf2154a
woorimj <forestwr06@gmail.com>
2024-07-11 07:21:31 +0900

    "remove git cache"

--- src/main/java/org/example/near/exception/model/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package org.example.near.exception.model;
-
-import org.example.near.exception.ErrorCode;
-
-public class BadRequestException extends CustomException{
-    public BadRequestException(ErrorCode errorCode, String message){
-        super(errorCode, message);
-    }
-}
--- src/main/java/org/example/near/exception/model/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package org.example.near.exception.model;
-
-import org.example.near.exception.ErrorCode;
-public class UnauthorizedException extends CustomException {
-    public UnauthorizedException(ErrorCode errorCode, String message) {
-        super(errorCode, message);
-    }
-}
